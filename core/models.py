9249a695498a3df55062bc3d4f19f58793578ec8
victorpajeu <pajeuvictor@gmail.com>
2020-03-25 18:28:17 -0300

    Updade core

--- core/models.py
+++ core/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class PontoTuristico(models.Model):
     nome = models.CharField('nome', max_length=100)
     descrição = models.TextField('descrição')
-    aprovado = models.BooleanField('status')
+    aprovado = models.BooleanField('aprovado')
 
     def __str__(self):
         return self.nome
