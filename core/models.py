bc74ec6faeb9c84cacb881ace44c59a3eb4993af
victorpajeu <pajeuvictor@gmail.com>
2020-03-25 18:53:23 -0300

    criação de app localização

--- core/models.py
+++ core/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+from atracoes.models import Atracao
 
 
 class PontoTuristico(models.Model):
     nome = models.CharField('nome', max_length=100)
     descrição = models.TextField('descrição')
     aprovado = models.BooleanField('aprovado')
+    atracao = models.ManyToManyField(Atracao)
 
     def __str__(self):
         return self.nome
