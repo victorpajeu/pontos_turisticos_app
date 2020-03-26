e9bb140bcf0a2303e8cf9af73e2823015b03fac8
victorpajeu <pajeuvictor@gmail.com>
2020-03-25 21:13:42 -0300

    update

--- core/models.py
+++ core/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from atracoes.models import Atracao
 from comentarios.models import Comentario
+from avaliacoes.models import Avaliacoes
 
 
 class PontoTuristico(models.Model):
@@ -9,6 +10,7 @@ class PontoTuristico(models.Model):
     aprovado = models.BooleanField('aprovado')
     atracao = models.ManyToManyField(Atracao)
     comentario = models.ManyToManyField(Comentario)
+    avaliacao = models.ManyToManyField(Avaliacoes)
 
     def __str__(self):
         return self.nome
