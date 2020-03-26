b1faac02d1ad868f9a55770483042fccc49c5252
victorpajeu <pajeuvictor@gmail.com>
2020-03-25 21:53:06 -0300

    remoção da app localização

--- avaliacoes/models.py
+++ avaliacoes/models.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import User
 class Avaliacoes(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     comentario = models.TextField()
+    nota = models.DecimalField(max_digits=5, decimal_places=2)
     aprovado = models.BooleanField()
     data = models.DateTimeField(auto_now_add=True)
 
--- localizacao/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class LocalizacaoConfig(AppConfig):
-    name = 'localizacao'
